f2053aadf4149dadd73100a8359783d5e61de542
email <email@iamaaronvarga.com>
2021-12-09 20:19:46 -0500

    header styling updates

--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -1,10 +1,14 @@
 // Import icons for nav
 import { MdPerson } from "react-icons/md";
+import { GiDolphin } from "react-icons/gi";
 
 export default function MyHeader() {
   return (
     <header style={styles.headerContainer}>
-      <h1 style={styles.h1}>My Wallet</h1>
+      <div style={styles.logoContainer}>
+        <GiDolphin style={styles.dolfinLogo} />
+        <h1 style={styles.h1}>Dolfin</h1>
+      </div>
       <nav style={styles.navContainer}>
         <MdPerson style={styles.headerAvatar} />
       </nav>
@@ -14,7 +18,6 @@ export default function MyHeader() {
 
 const styles = {
   headerContainer: {
-    color: "#000",
     padding: "1rem",
     backgroundColor: "#fff",
     display: "flex",
@@ -27,18 +30,29 @@ const styles = {
     display: "flex",
     alignItems: "center",
   },
+  logoContainer: {
+    display: "flex",
+  },
   h1: {
     margin: "0",
     padding: "0",
     lineHeight: "1.5rem",
     fontSize: "1.5rem",
+    color: "#333",
+  },
+  dolfinLogo: {
+    fontSize: "1.5rem",
+    marginRight: "-.75rem",
+    marginTop: "-.15rem",
+    color: "#fbb1bd",
+    zIndex: "1",
   },
   headerAvatar: {
     fontSize: "1.5rem",
     cursor: "pointer",
     padding: ".5rem",
     borderRadius: "50%",
-    backgroundColor: "#000",
+    backgroundColor: "#333",
     color: "#fff",
   },
 };
